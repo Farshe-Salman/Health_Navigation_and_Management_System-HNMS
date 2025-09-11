d877e396b215b93400d8b39c780dc338a47fd9f5
Miyoko Mahzabin Mim <miyokomahzabin29@gmail.com>
2025-09-12 05:12:39 +0600

    Add files via upload

---
+++ view/footer.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/landingpage.css">
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+    <p>© 2025 Health Navigation Management System | All Rights Reserved</p>
+    <p>Contact: support@hnms.com | Phone: +8801303672091</p>
+  </footer>
+</body>
+</html>
